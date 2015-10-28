0f05017fd2c2aa9d2effbf65317eec273a86a79a
chkt <ck@christoph-kettelhoit.de>
2015-10-28 21:23:13 +0100

    Updated distribution

--- dist/ConfigProvider.js
+++ dist/ConfigProvider.js
@@ -8,8 +8,6 @@ var _createClass = (function () { function defineProperties(target, props) { for
 
 var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj['default'] = obj; return newObj; } }
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
@@ -19,10 +17,6 @@ function _inherits(subClass, superClass) { if (typeof superClass !== 'function' 
 var _Provider2 = require('./Provider');
 
 var _Provider3 = _interopRequireDefault(_Provider2);
-
-var _BaseObservable = require('./BaseObservable');
-
-var _observe = _interopRequireWildcard(_BaseObservable);
 
 var _CFG_PROTO = Object.freeze({
 	set: function set(props) {
@@ -78,6 +72,15 @@ var ConfigProvider = (function (_Provider) {
 			}
 
 			return _get(Object.getPrototypeOf(ConfigProvider.prototype), 'get', this).call(this, _symbol.get(cns));
+		}
+	}, {
+		key: 'getUnique',
+		value: function getUnique() {
+			var sym = Symbol();
+
+			_reverse[sym] = function () {};
+
+			return _factory.call(this, sym);
 		}
 	}]);
 
