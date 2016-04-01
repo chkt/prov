8808d5d788d73d1a0e43d6835ba227c5f001a83c
chkt <ck@christoph-kettelhoit.de>
2016-04-01 01:37:29 +0100

    Updated tasks

--- gulp/task/build.js
+++ gulp/task/build.js
@@ -4,9 +4,9 @@ import _es6 from 'gulp-babel';
 
 
 
-_gulp.task('build', ['clean'], () => {
-	return _gulp
+_gulp.task('build', ['clean'], () =>
+	_gulp
 		.src('source/**/*.js')
 		.pipe(_es6())
-		.pipe(_gulp.dest('dist'));
-});
+		.pipe(_gulp.dest('es5'))
+);
